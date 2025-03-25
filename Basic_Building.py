ebc1d0131f29c253dfe52de605dcac14680f7caa
Neeraj Prajapati <137681918+nrajprajapati@users.noreply.github.com>
2025-03-25 22:40:52 +0530

    Neeraj
    Basic Building Revision

---
+++ Basic_Building.py
@@ -0,0 +1,35 @@
+x=5
+y=3
+x=y
+y=x+2
+print(x,y)
+
+a=10
+b=5
+c=a//b
+d=a%b
+print(c,d)
+
+x=5
+y=10
+x=y
+y=20
+print(x)
+
+x=5
+y=10
+if x > y:
+    print("x is greater")
+else:
+    print("y is greater")
+
+sum=0
+for i in range(1,6):
+    sum+=i
+print(sum)
+
+count = 0
+while count < 5:
+    print(count)
+    count+=1
+
